8d842f25db0df361529e45b9ea36a4ba88c3b695
Zhukouskin <zhukouskin@gmail.com>
2021-02-17 23:46:30 +0300

    Task_4_Ex_1

---
+++ src/Ex_1/Main.java
@@ -0,0 +1,38 @@
+package Ex_1;
+/*
+В английском тексте каждую букву заменить ее порядковым номером в алфавите.
+При выводе в одной строке печатать текст с двумя пробелами между буквами,
+в следующей строке внизу под каждой буквой печатать ее номер.
+ */
+
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String str = "Where there is muck there is brass".replace(" ", "").toLowerCase(); // Создали строку, убрали из неё пробелы и сделали все буквы одного размера (строчными).
+        char[] strToChar = str.toCharArray();// Перевели строку с текстом в массив , для дальнейшего сопостовления с массивом алфавита.
+        int[] index = new int[strToChar.length];// Создали массив для индекса букв текста.
+        char[] AlfaArray = new char[26];
+        for (int i = 0; i < AlfaArray.length; i++) {
+            AlfaArray[i] = (char) (97 + i); // Создали и заполнили массив алфавита.
+        }
+        for (int i = 0; i < strToChar.length; i++) {     //Заполнили массив index
+            for (int j = 0; j < AlfaArray.length; j++) { //порядковыми номерами
+                if (AlfaArray[j] == strToChar[i]) {      //букв текста
+                    index[i] = (AlfaArray[j] - 96);
+                }
+            }
+        }
+
+        for (int i = 0; i < strToChar.length; i++) {
+            System.out.printf("%3c", strToChar[i]);
+
+        }
+        System.out.println();
+
+        for (int i = 0; i < index.length;i++){
+            System.out.printf("%3d", index[i]);
+        }
+    }
+}
